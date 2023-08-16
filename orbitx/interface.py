754a78ff90b74b67347a894e4df7f18f20f2f711
Mattea Goalen <mattea.goalen@npl.co.uk>
2023-08-16 13:29:24 +0100

    remove plotting

--- orbitx/interface.py
+++ orbitx/interface.py
@@ -63,36 +63,15 @@ def return_matchups(
         interpolation_sampling_interval,
     )
 
-    match = matchup(orbit_output, time_diff_threshold,
+    # find matchups between orbits
+    match_ups = matchup(orbit_output, time_diff_threshold,
                     interpolation_sampling_interval,
                     cntr2cntr_dist)
 
     # save orbital data
     # orbit.save(orbit_output, output_path_sim_orbits)
-    import matplotlib.pyplot as plt
-    lon1 = [j for i, j in enumerate(match["S3A_LS8"]["lon1"]) if -60 <= match["S3A_LS8"]["lat1"][i] <= 60]
-    lat1 = [j for i, j in enumerate(match["S3A_LS8"]["lat1"]) if -60 <= match["S3A_LS8"]["lat1"][i] <= 60]
-
-    lon2 = [j for i, j in enumerate(match["S3A_LS8"]["lon2"]) if -60 <= match["S3A_LS8"]["lat1"][i] <= 60]
-    lat2 = [j for i, j in enumerate(match["S3A_LS8"]["lat2"]) if -60 <= match["S3A_LS8"]["lat1"][i] <= 60]
-
-    plt.scatter(lon1, lat1)
-    plt.scatter(lon2, lat2)
-    plt.show()
-
-    # find matchups between orbits
-    matchups = MatchUp()
-    matchup_output = matchups.search(
-        orbit_output,
-        latmin,
-        latmax,
-        lonmin,
-        lonmax,
-        interpolation_sampling_interval,
-        time_diff_threshold,
-    )
 
     # save matchup data
-    fname = f"matchup_{'_'.join(list(orbit_output.keys()))}_starttime_{start_time.strftime('%Y%m%d')}_endtime_{end_time.strftime('%Y%m%d')}_samplinginterval_{propagation_sampling_interval}_tmptol_{time_diff_threshold}.txt"
+    # fname = f"matchup_{'_'.join(list(orbit_output.keys()))}_starttime_{start_time.strftime('%Y%m%d')}_endtime_{end_time.strftime('%Y%m%d')}_samplinginterval_{propagation_sampling_interval}_tmptol_{time_diff_threshold}.txt"
 
-    matchups.save(matchup_output, output_path_matchups, fname)
+    # matchups.save(matchup_output, output_path_matchups, fname)
