ed12def976fe17d0f41b7687322a071a61f60469
Mattea Goalen <mattea.goalen@npl.co.uk>
2023-08-18 18:53:03 +0100

    add test structure

--- orbitx/tests/test_matchup.py
+++ orbitx/tests/test_matchup.py
@@ -1,19 +1,38 @@
 import unittest
+import unittest.mock as mock
 
 import numpy as np
 
-from orbitx.matchup_test import matchup
+from orbitx.matchup import Matchups
 
 
-class MyTestCase(unittest.TestCase):
-    def test_matchup(self):
-        input_orbits = {"S3A":
-                            {"lat": np.arange(-10, 10), "lon": (np.arange(-10, 10)**2)-30, "time": [float(i) for i in np.arange(20)]},
-                        "LS8":
-                            {"lat": np.arange(-10, 10), "lon": -(np.arange(-10, 10)**2), "time": [float(i) for i in np.arange(20)]}
-                        }
+class TestMatchups(unittest.TestCase):
 
-        self.assertEqual(matchup(input_orbits, 3, 1, 400), ())
+    def test_get_distance(self):
+        pass
+
+    @mock.patch("orbitx.matchup.get_distance")
+    def test_matchup(self, mock_get_distance):
+        input_orbits = {
+            "S3A": {
+                "lat": np.arange(-10, 10),
+                "lon": (np.arange(-10, 10) ** 2) - 30,
+                "time": [float(i) for i in np.arange(20)],
+            },
+            "LS8": {
+                "lat": np.arange(-10, 10),
+                "lon": -(np.arange(-10, 10) ** 2),
+                "time": [float(i) for i in np.arange(20)],
+            },
+        }
+        matchup = Matchups()
+        self.assertEqual(matchup.matchup(input_orbits, 3, 1, 400), ())
+
+    def test_to_ds(self):
+        pass
+
+    def test_save(self):
+        pass
 
 
 if __name__ == "__main__":
